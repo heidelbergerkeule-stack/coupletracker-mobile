da2157b769f07ba163882716b0e5ca5731b12608
heidelbergerkeule-stack <heidelbergerkeule@gmail.com>
2025-11-16 19:13:14 +0100

    Add App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>CoupleTracker Mobile</Text>
+      <Text>Setup: Siehe SETUP_GUIDE.md</Text>
+    </View>
+  );
+}
+
+export default App;
